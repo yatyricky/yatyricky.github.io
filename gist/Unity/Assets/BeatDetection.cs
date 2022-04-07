fe8ac132e95dd102a48486b846ba7fd9b26e8ccd
Rick Sun <yatyricky@gmail.com>
2022-04-07 15:25:29 +0800

    1

--- gist/Unity/Assets/BeatDetection.cs
+++ gist/Unity/Assets/BeatDetection.cs
@@ -6,41 +6,107 @@ using UnityEngine;
 
 public class BeatDetection : MonoBehaviour
 {
-    public float[] samples;
-    public float max = float.NegativeInfinity;
-    public float min = float.PositiveInfinity;
+    public int blockSize = 1024;
+
+    private float[][] _samples;
+
+    public float[] max;
+    public float[] min;
+
+    public int channelsCount = 2;
+
+    public Transform[] channels;
+    public Material[] materials;
+
+    public int sampleRate;
+
+    private int _sampleBlocks;
+    private float[] _frameEnergy;
+    private int _frameIndex;
+
+    public Transform averageE;
 
     private AudioSource _audioSource;
 
-    // Start is called before the first frame update
-    void Start()
+    public void ComputeSoundEnergy()
     {
-        samples = new float[1024];
-        _audioSource = GetComponent<AudioSource>();
-        var trs = transform;
-        for (int i = 0; i < 1024; i++)
+        var e = 0f;
+        for (int i = 0; i < blockSize; i++)
         {
-            var obj = GameObject.CreatePrimitive(PrimitiveType.Cube);
-            obj.transform.SetParent(trs);
+            var sum = 0f;
+            for (int j = 0; j < channelsCount; j++)
+            {
+                var sample = _samples[j][i];
+                sum += Mathf.Pow(sample, 2);
+            }
+
+            e += sum;
+        }
+
+        _frameEnergy[_frameIndex++ % _sampleBlocks] = e;
+    }
+
+    public void UpdateAverageE()
+    {
+        var sum = 0f;
+        for (int i = 1; i <= _sampleBlocks; i++)
+        {
+            sum += _frameEnergy[(_frameIndex + i) % _sampleBlocks];
+        }
+
+        sum /= _sampleBlocks;
+
+        var height = sum * 3f;
+        averageE.localScale = new Vector3(10f, height, 1f);
+        var cpos = averageE.localPosition;
+        averageE.localPosition = new Vector3(cpos.x, height / 2f, cpos.z);
+    }
+
+    // Start is called before the first frame update
+    private void Start()
+    {
+        _sampleBlocks = Mathf.FloorToInt(sampleRate / blockSize);
+        _frameEnergy = new float[_sampleBlocks];
+
+        _samples = new float[channelsCount][];
+        max = new float[channelsCount];
+        min = new float[channelsCount];
+        _audioSource = GetComponent<AudioSource>();
+        for (var c = 0; c < channelsCount; c++)
+        {
+            _samples[c] = new float[blockSize];
+            max[c] = -99999999f;
+            min[c] = 99999999f;
+            for (var i = 0; i < blockSize; i++)
+            {
+                var obj = GameObject.CreatePrimitive(PrimitiveType.Cube);
+                obj.GetComponent<Renderer>().material = materials[c];
+                obj.transform.SetParent(channels[c]);
+            }
         }
     }
 
     // Update is called once per frame
-    void Update()
+    private void Update()
     {
-        var trs = transform;
-        _audioSource.GetOutputData(samples, 0);
-        max = Mathf.Max(samples.Max(), max);
-        min = Mathf.Min(samples.Min(), min);
-        for (int i = 0; i < 1024; i++)
+        for (var c = 0; c < channelsCount; c++)
         {
-            var s = samples[i];
-            var x = (float) i; // i % 32;
-            var y = 0f; //i / 32;
-            var obj = trs.GetChild(i);
-            var h = (1f - (s + 1) / 2f) * 500f;
-            obj.localScale = new Vector3(1, h, 1);
-            obj.localPosition = new Vector3(x, h / 2, y);
+            _audioSource.GetOutputData(_samples[c], c);
+            max[c] = Mathf.Max(_samples[c].Max(), max[c]);
+            min[c] = Mathf.Min(_samples[c].Min(), min[c]);
+            for (var i = 0; i < blockSize; i++)
+            {
+                var s = _samples[c][i];
+                var x = (float) i; // i % 32;
+                var y = 0f; //i / 32;
+                var obj = channels[c].GetChild(i);
+                var h = (1f - (s + 1) / 2f) * 500f;
+                obj.localScale = new Vector3(1, h, 1);
+                obj.localPosition = new Vector3(x, h / 2, y);
+            }
         }
+
+        ComputeSoundEnergy();
+        UpdateAverageE();
     }
 }
