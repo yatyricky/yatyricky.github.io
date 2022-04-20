2e913162166a23dc1c97dbfa1b8ad028c7780e90
Rick Sun <yatyricky@gmail.com>
2022-04-20 09:45:33 +0800

    1

--- gist/Lua/compare.lua
+++ gist/Lua/compare.lua
@@ -6,7 +6,7 @@ require("gist/Lua/lib/compat")
 local left = require("gist/Lua/left").StoreByID
 local right = require("gist/Lua/right").StoreByID
 
-local function leftProcess(t)
+local function processTab(t)
     local keys = table.keys(t)
     table.sort(keys)
     return table.concat(table.imap(keys, function(i, v)
@@ -14,13 +14,7 @@ local function leftProcess(t)
     end), ";")
 end
 
-for key, v in pairs(left) do
-    v.Items = leftProcess(v.Items)
-    v.BetterItems = leftProcess(v.BetterItems)
-    v.FirstItems = leftProcess(v.FirstItems)
-end
-
-local function rightProcess(t)
+local function processKV(t)
     table.sort(t, function(a,b)
         return a.k < b.k
     end)
@@ -29,10 +23,28 @@ local function rightProcess(t)
     end), ";")
 end
 
+local function unaryProcess(t)
+    if table.isEmpty(t) then
+        return t
+    end
+    local firstK, firstV = next(t)
+    if type(firstV) == "table" then
+        return processKV(t)
+    else
+        return processTab(t)
+    end
+end
+
+for key, v in pairs(left) do
+    v.Items = unaryProcess(v.Items)
+    v.BetterItems = unaryProcess(v.BetterItems)
+    v.FirstItems = unaryProcess(v.FirstItems)
+end
+
 for _, v in pairs(right) do
-    v.Items = rightProcess(v.Items)
-    v.BetterItems = rightProcess(v.BetterItems)
-    v.FirstItems = rightProcess(v.FirstItems)
+    v.Items = unaryProcess(v.Items)
+    v.BetterItems = unaryProcess(v.BetterItems)
+    v.FirstItems = unaryProcess(v.FirstItems)
 end
 
 table.compare(left, right)
